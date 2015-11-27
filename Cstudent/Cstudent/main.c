5ffcbb35950d7af7d8da6204cfb43367afc154b2
fuok <liuyang@Browns-Mac.local>
2015-11-27 14:35:07 +0800

    创建

---
+++ Cstudent/Cstudent/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Cstudent
+//
+//  Created by 刘阳 on 15/11/27.
+//  Copyright © 2015年 刘阳. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
